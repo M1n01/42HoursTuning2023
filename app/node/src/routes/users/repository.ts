4c257cd6ed1e324c8fb7b7d279a5990da62027d3
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2023-06-18 05:31:50 +0900

    fix: n+1

--- app/node/src/routes/users/repository.ts
+++ app/node/src/routes/users/repository.ts
@@ -50,31 +50,24 @@ export const getUsers = async (
 export const getUserByUserId = async (
   userId: string
 ): Promise<User | undefined> => {
-  const [user] = await pool.query<RowDataPacket[]>(
-    "SELECT user_id, user_name, office_id, user_icon_id FROM user WHERE user_id = ?",
-    [userId]
-  );
+  const query = `SELECT u.user_id, u.user_name, o.office_name, u.user_icon_id, f.file_name FROM user AS u \
+    LEFT JOIN office AS o ON o.office_id = u.office_id \
+    LEFT JOIN file AS f ON f.file_id = u.user_icon_id \
+    WHERE u.user_id = '${userId}'`;
+
+  const [user] = await pool.query<RowDataPacket[]>(query);
   if (user.length === 0) {
     return;
   }
-
-  const [office] = await pool.query<RowDataPacket[]>(
-    `SELECT office_name FROM office WHERE office_id = ?`,
-    [user[0].office_id]
-  );
-  const [file] = await pool.query<RowDataPacket[]>(
-    `SELECT file_name FROM file WHERE file_id = ?`,
-    [user[0].user_icon_id]
-  );
 
   return {
     userId: user[0].user_id,
     userName: user[0].user_name,
     userIcon: {
       fileId: user[0].user_icon_id,
-      fileName: file[0].file_name,
+      fileName: user[0].file_name,
     },
-    officeName: office[0].office_name,
+    officeName: user[0].office_name,
   };
 };
 
@@ -114,7 +107,7 @@ export const getUsersByTargets = async (
 ): Promise<SearchedUser[]> => {
   // クエリを生成する
   let query = "";
-  let isFirst = true;
+  const isFirst = true;
   for (const target of targets) {
     if (isFirst) {
       query += `SELECT user_id FROM user WHERE ${target} LIKE '%${target}%'`;
@@ -261,11 +254,11 @@ export const getUserForFilter = async (
 ): Promise<UserForFilter> => {
   let userRows: RowDataPacket[];
   if (!userId) {
-      const offset = Math.floor(Math.random() * 300001);
-      [userRows] = await pool.query<RowDataPacket[]>(
-        "SELECT user_id, user_name, office_id, user_icon_id FROM user LIMIT 1 OFFSET ?",
-        [offset]
-      );
+    const offset = Math.floor(Math.random() * 300001);
+    [userRows] = await pool.query<RowDataPacket[]>(
+      "SELECT user_id, user_name, office_id, user_icon_id FROM user LIMIT 1 OFFSET ?",
+      [offset]
+    );
   } else {
     [userRows] = await pool.query<RowDataPacket[]>(
       "SELECT user_id, user_name, office_id, user_icon_id FROM user WHERE user_id = ?",
