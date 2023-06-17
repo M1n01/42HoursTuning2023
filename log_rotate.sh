3e6e7efc0537e3fa0ba2c0d1bfb6f0b6c952cd37
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2023-06-17 18:54:48 +0900

    chore: log rotate用のスクリプトを作成

---
+++ log_rotate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ログのローテート
+mv ./app/logs/nginx/access.log "./app/logs/nginx/access.log.$(date "+%Y%m%d_%H%M%S")"
+mv ./app/logs/nginx/error.log "./app/logs/nginx/error.log.$(date "+%Y%m%d_%H%M%S")"
+mv ./app/logs/mysql/slow.log "./app/logs/mysql/slow.log.$(date "+%Y%m%d_%H%M%S")"
