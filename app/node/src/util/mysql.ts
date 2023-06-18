dcbe86c058167c41ec79b53715c543a9f56b44e2
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2023-06-18 13:25:27 +0900

    sysがすこしあったので200に減らしてみる

--- app/node/src/util/mysql.ts
+++ app/node/src/util/mysql.ts
@@ -6,7 +6,7 @@ const mysqlOption: PoolOptions = {
   password: "mysql",
   database: "app",
   waitForConnections: true,
-  connectionLimit: 275,
+  connectionLimit: 200,
   enableKeepAlive: true,
 };
 
