95ad03676eee0d9e13e0dfed2f91ebe944c6bac2
Ayato Hayashi <ayato.hayashi@justincase-tech.com>
2023-06-18 12:44:22 +0900

    connection: 100

--- app/node/src/util/mysql.ts
+++ app/node/src/util/mysql.ts
@@ -6,7 +6,7 @@ const mysqlOption: PoolOptions = {
   password: "mysql",
   database: "app",
   waitForConnections: true,
-  connectionLimit: 20,
+  connectionLimit: 100,
   enableKeepAlive: true,
 };
 
